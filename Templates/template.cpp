06342812a6c0baa4edee77858fe72f8ca2ed5778
ipatheticoi <rhuauyam@uni.pe>
2020-03-22 00:02:42 -0500

    update

--- Templates/template.cpp
+++ Templates/template.cpp
@@ -10,9 +10,6 @@ typedef long long ll;
 #define print(x) cout<<x<<endl;
 using namespace std;
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     return 0;
 }
